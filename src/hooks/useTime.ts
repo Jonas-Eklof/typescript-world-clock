4c22fc2c5c006db3bee7b119222fbb63ea978542
Jonas E <jonas_eklof89@hotmail.com>
2025-09-12 14:30:27 +0200

    lade till kommentarer

--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 
+// get the current time for Sweden
 const getTimeForZone = (timeZone: string): Date => {
   return new Date(new Date().toLocaleString("sv-SE", { timeZone }));
 };
 
+// custom react hook that returns the current time for a given timezone
+// the setInterval updates the time every second
 export const useTime = (timeZone: string) => {
   const [time, setTime] = useState<Date>(() => getTimeForZone(timeZone));
 
